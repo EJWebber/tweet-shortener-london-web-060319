49660733bcb74ff6ca2fd044ac5e5c7f314056ce
Ed Webber <edwebber06@gmail.com>
2019-05-28 15:08:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 def word_substituter(tweet)
   dictionary
   tweet_array = tweet.split(' ')
-  tweet_array.maps do |word|
+  tweet_array.map do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
   end
